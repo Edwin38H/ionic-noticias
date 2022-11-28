b1f53a6cd25a0b405b31f44c31eadc56f81ffe14
Edwin38H <djerizooriginal@gmail.com>
2022-11-27 22:05:05 -0600

    practica 35

--- src/app/pages/tab3/tab3.module.ts
+++ src/app/pages/tab3/tab3.module.ts
@@ -3,10 +3,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
-
 @NgModule({
   imports: [
     IonicModule,
@@ -17,4 +15,4 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
   ],
   declarations: [Tab3Page]
 })
-export class Tab3PageModule {}
+export class Tab3PageModule { }
